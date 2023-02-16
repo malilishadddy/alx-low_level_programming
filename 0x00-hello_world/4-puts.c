00a598a3e92a71f96bf125bf76fff35553b24a6e
malilishadddy <malilishaddy@gmail.com>
2023-02-16 01:24:23 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+
+
+/**
+ * main -prints "Programming is like building a multilingual puzzle"
+ * return 0.
+ */
+int main()
+{
+	puts ("\"Programming is like building a multilinguil puzzle);
+	return(0);
+}
