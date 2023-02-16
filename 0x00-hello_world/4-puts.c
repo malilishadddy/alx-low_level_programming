2ef7ce785a4dc0e44001cc497f0a657376b3a101
malilishadddy <124892968+malilishadddy@users.noreply.github.com>
2023-02-16 22:45:33 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
  * Return:Always 0.
  */
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilinguil puzzle");
-
-	return (0);
+puts ("\"Programming is like building a multilinguil puzzle");
+return (0);
 }
