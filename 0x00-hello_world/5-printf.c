c4fb84eef3ce0ffef4aad7614f38d99cf018e8df
malilishadddy <124892968+malilishadddy@users.noreply.github.com>
2023-02-16 22:55:12 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 /**
- * main prints "with proper grammar, but the outcome is but the outcome is a piece 
- * of art,"
+ * main prints "with proper grammar, but the outcome is
+ * a piece of art,"
  *  Return: Always 0.
  *
  */
