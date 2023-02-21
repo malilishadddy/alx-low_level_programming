f91b2d0de2d6976164508efb8bf2af423c6763af
malilishadddy <124892968+malilishadddy@users.noreply.github.com>
2023-02-21 23:30:21 +0300

    taskall

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,20 +1,12 @@
 
 #include "main.h"
 		
-
-		
-/**
-		
- * main - check the code.
-		
- *
-		
- * Return: Always 0.
-		
- */
-		
-int main(void)
-		
+/**		
+ * main - check the code.		
+ *	
+ * Return: Always 0.		
+ */		
+int main(void)	
 {
 		
     int r;
