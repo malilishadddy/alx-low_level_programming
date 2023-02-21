21e4260ca12e9042dcc528f7d6a3ce2a829bc7f0
malilishadddy <124892968+malilishadddy@users.noreply.github.com>
2023-02-21 22:58:43 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,6 +8,11 @@
 	 * @n: print from this number
 	 */
 	void print_to_98(int n)
+	/**
+	 * print_to_98 - prints all natural numbers from n to 98,
+	 * followed by a new line
+	 * @n: print from this number
+	 */
 	{
 		int i, j;
 
